81d20493c7a3fd6efc6b14fe34600b3663c39be1
icegoraebab <ohw2509@gmail.com>
2025-07-24 19:25:52 +0900

    Register SSH key and update remote URL

---
+++ sim_ws/src/pid_controller/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'pid_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hyeonwoo',
+    maintainer_email='ohw2509@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'pid_speed_control = pid_controller.pid_speed_control:main',
+        ],
+    },
+)
